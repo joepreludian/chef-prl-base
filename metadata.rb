b4c49bac7cf4a98d6d2de33adce5099ae3823bac
Jonhnatha Trigueiro <joepreludian@gmail.com>
2019-08-25 20:31:17 -0300

    Update docker compose with a guard not_if

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joepreludian@gmail.com'
 license 'MIT'
 description 'Installs/Configures prl_base'
 long_description 'Installs/Configures prl_base'
-version '0.2.1'
+version '0.2.2'
 chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
--- recipes/install_docker.rb
+++ recipes/install_docker.rb
@@ -9,6 +9,7 @@ end
 execute 'install_docker_compose' do
   command 'curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && /usr/local/bin/docker-compose version'
   live_stream true
+  not_if { ::File.exist?('/usr/local/bin/docker-compose') }
 end
 
 execute 'docker_post_actions' do
